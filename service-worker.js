b64d614ac802a22939cb0437d58b9e79e49ff668
ALEEF02 <thesmarty2002@gmail.com>
2020-03-01 22:49:25 -0800

    Change cache version, add robots.txt to .forgerc

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v3.7.1';
-const RUNTIME = 'runtime-v3.7.1';
+const PRECACHE = 'precache-v3.7.2';
+const RUNTIME = 'runtime-v3.7.2';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
